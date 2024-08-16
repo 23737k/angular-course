bfa1264c64897211bc1c09fddead11596100c2ad
Kenti Antezana <mopup2017@gmail.com>
2024-08-16 13:02:32 -0300

    Binding e Event Binding. Ejemplo básico.

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -10,5 +10,17 @@ export class EmpleadoComponent {
   apellido= 'Perez'
   empresa= ''
   edad=26
+  private sexo=''
+
+  setSexo(event:Event){
+    //Mediante target podemos acceder al elemento html del evento. 
+    //Primero lo casteamos al elemento correspondiente con <>
+    let input   = <HTMLInputElement> event.target; 
+    //Le asignamos el value del radio button a la propiedad sexo
+    this.sexo = input.value;   
+  }
+  getSexo(){
+    return this.sexo;
+  }
 
 }
